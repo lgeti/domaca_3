b578d3df3bba21a96f9ee78f9055d6fbc9d08273
lgeti <ignatpocevski@gmail.com>
2023-05-30 22:55:16 +0200

    Logo added

--- navigation.php
+++ navigation.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
     <div class="container-fluid">
+        <a class="navbar-brand" href="/domaca_3/index.php/group"><img src="\domaca_3\assets\logo.png" alt="Recipe.si Logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
