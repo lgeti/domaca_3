9fa636950b2e32d001253bac6ee9c47bef82a4bd
lgeti <ignatpocevski@gmail.com>
2023-05-30 18:46:54 +0200

    Chat implemented

--- Model/ChatDB.php
+++ Model/ChatDB.php
@@ -3,38 +3,42 @@
 require_once "DBInit.php";
 
 class ChatDB {
-    public static function insertMessage($chatId, $userId, $message) {
+
+    public static function getAll() {
         $db = DBInit::getInstance();
 
-        $statement = $db->prepare("INSERT INTO message (chat_id, user_id, content, created_at)
-            VALUES (:chatId, :userId, :content, NOW())");
-        $statement->bindParam(":chatId", $chatId, PDO::PARAM_INT);
-        $statement->bindParam(":userId", $userId, PDO::PARAM_INT);
-        $statement->bindParam(":content", $message);
-        $statement->execute();
-    }
-
-    public static function getMessagesByChat($chatId) {
-        $db = DBInit::getInstance();
-
-        $statement = $db->prepare("SELECT id, chat_id, user_id, content, created_at 
-            FROM message WHERE chat_id = :chatId");
-        $statement->bindParam(":chatId", $chatId, PDO::PARAM_INT);
+        $statement = $db->prepare("SELECT id, date, user, message
+                                     FROM chat
+                                 ORDER BY date ASC");
         $statement->execute();
 
         return $statement->fetchAll();
     }
-    
-    public static function getChatsByUser($userId) {
+
+    public static function getAllSinceId($id) {
         $db = DBInit::getInstance();
 
-        $statement = $db->prepare("SELECT chat.id, chat.name, chat.description
-            FROM chat
-            INNER JOIN chat_user ON chat.id = chat_user.chat_id
-            WHERE chat_user.user_id = :userId");
-        $statement->bindParam(":userId", $userId, PDO::PARAM_INT);
+        $statement = $db->prepare("SELECT id, date, user, message FROM chat
+                                    WHERE id > :id ORDER BY date ASC");
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
         $statement->execute();
 
         return $statement->fetchAll();
     }
+
+    public static function insert($user, $message) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("INSERT INTO chat (user, message) VALUES (:user, :message)");
+        $statement->bindParam(":user", $user);
+        $statement->bindParam(":message", $message);
+        $statement->execute();
+    }
+
+    public static function deleteAll() {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("TRUNCATE chat");
+        $statement->execute();
+    }   
 }
